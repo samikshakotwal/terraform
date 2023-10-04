9c26802693b6cfe480a81c9ab25779827c7b1d9b
samikshakotwal <89957781+samikshakotwal@users.noreply.github.com>
2023-10-04 10:22:04 +0530

    Update main.tf2

--- main.tf
+++ main.tf
@@ -1,17 +1,62 @@
 terraform {
   required_providers {
-    mycloud = {
-      source  = "mycorp/mycloud"
-      version = "~> 1.0"
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
     }
   }
 }
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
 provider "aws" {
-  access_key="var.accesskey"
-  secret_key= "var.secretkey"
-  region     = "ap-south-1"
+access_key = var.AWS_ACCESS_KEY
+secret_key = var.AWS_SECRET_KEY
+region = "ap-south-1"
 }
-resource "aws_key_pair" "tf-key-pair" {
+ 
+resource "aws_instance" "myec2" {
+  ami           = "ami-03a6eaae9938c858c"
+  instance_type = "t2.micro"
+  vpc_security_group_ids=[aws_security_group.web-sg.id]
+  key_name="tf-key-pair"
+ 
+ 
+tags={
+ Name="web-server"
+}
+user_data= <<-EOF
+#!/bin/bash
+yum install httpd -y
+service httpd start
+cd /var/www/html
+touch index.html
+echo "hello from Terraform" > index.html
+EOF
+}
+resource "aws_security_group" "web-sg" {
+ name="web-sg"
+ingress {
+ from_port=80
+ to_port=80
+protocol="tcp"
+cidr_blocks= ["0.0.0.0/0"]
+}
+ 
+ 
+ingress {
+ from_port=22
+ to_port=22
+protocol="tcp"
+cidr_blocks= ["0.0.0.0/0"]
+}
+egress {
+ from_port=0
+ to_port=0
+protocol="-1"
+cidr_blocks= ["0.0.0.0/0"]
+}
+}
+resource "aws_key_pair" "tf-key-pair-1" {
 key_name = "tf-key-pair"
 public_key = tls_private_key.rsa.public_key_openssh
 }
@@ -21,25 +66,5 @@ rsa_bits  = 4096
 }
 resource "local_file" "tf-key" {
 content  = tls_private_key.rsa.private_key_pem
-filename = "tf-key-pair"
+filename = "tf-key-pair-1"
 }
-module "vpc"{
-source= "./vpcmodule/"
-}
-module "web" {
-source= "./webmodule/"
-subnet_id=module.vpc.websubnet_id
-vpc_id=module.vpc.vpcid
-}
-module "app"{
-source= "./appmodule/"
-subnet_id=module.vpc.appsubnet_id
-vpc_id=module.vpc.vpcid
-}
-module "db"{
-source= "./dbmodule/"
-vpc_id= module.vpc.vpcid
-subnet_id= module.vpc.dbsubnet_id
-appsubnetid= module.vpc.appsubnet_id
-}
-
